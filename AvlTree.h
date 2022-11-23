a914b5b7479d1d1de3d00181bd4c1dd95fce3efb
danashabtay <99danashabtay@gmail.com>
2022-11-23 14:27:00 +0200

    added AVL functions

--- AvlTree.h
+++ AvlTree.h
@@ -5,16 +5,43 @@
 #ifndef WET1_AVLTREE_H
 #define WET1_AVLTREE_H
 
+#include <iostream>
+using namespace std;
+
+struct node {
+    struct node *left;
+    int data;
+    int height;
+    struct node *right;
+};
+
 template<class T>
 class AvlTree {
+
 public:
-    AvlTree();
+    struct node * root;
+    AvlTree(){
+        this->root = NULL;
+    }
     virtual ~AvlTree();
     virtual T* addToTree(T type);
     virtual void removeFromTree(T type);
 
+    int calHeight(struct node *p);
+    int bf(struct node *n);
+    struct node * llRotation(struct node *n);
+    struct node * rrRotation(struct node *n);
+    struct node * rlRotation(struct node *n);
+    struct node * lrRotation(struct node *n);
 
-};
+    struct node* insert(struct node *r,int data);
+
+    void levelorder_newline();
+    void levelorder_newline(struct node *v);
+
+    struct node * deleteNode(struct node *p,int data);
+    struct node* inpre(struct node* p);
+    struct node* insuc(struct node* p);
 
 
 #endif //WET1_AVLTREE_H
