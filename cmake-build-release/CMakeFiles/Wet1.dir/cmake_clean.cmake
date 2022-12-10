10a2cd0ac5ab168d15c9e3d33901372438cac436
nivarviv <102030460+nivarviv@users.noreply.github.com>
2022-12-10 12:08:41 +0200

    /

---
+++ cmake-build-release/CMakeFiles/Wet1.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Wet1.dir/main23a1.cpp.obj"
+  "CMakeFiles/Wet1.dir/player.cpp.obj"
+  "CMakeFiles/Wet1.dir/playerStats.cpp.obj"
+  "CMakeFiles/Wet1.dir/team.cpp.obj"
+  "CMakeFiles/Wet1.dir/worldcup23a1.cpp.obj"
+  "Wet1.exe"
+  "Wet1.exe.manifest"
+  "Wet1.pdb"
+  "libWet1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Wet1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
